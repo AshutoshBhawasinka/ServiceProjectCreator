a1919f6b8f782fbc733a122c588e26936c634720
Ashutosh Bhawasinka <ashutosh.kr@gmail.com>
2025-05-12 06:56:49 +0530

    remove copyright

--- Template/ServiceInstaller.cs
+++ Template/ServiceInstaller.cs
@@ -1,13 +1,3 @@
-﻿#region (C) Koninklijke Philips N.V. 2018
-//
-// All rights are reserved. Reproduction or transmission in whole or in part,
-// in any form or by any means, electronic, mechanical or otherwise, is
-// prohibited without the prior written permission of the copyright owner.
-//
-// Filename: ServiceInstaller.cs
-//
-#endregion
-
 using System.ComponentModel;
 
 namespace $NAMESPACE$
